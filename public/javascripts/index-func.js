348700fe49ca5f18fb7635344dfeadc5291ae47e
Avery Wells <avewells@gmail.com>
2018-02-14 22:31:26 -0600

    moved dashboard logic to jquery. fixed date formatting

--- public/javascripts/index-func.js
+++ public/javascripts/index-func.js
@@ -21,6 +21,30 @@ function sortPosts(order) {
     }
 }
 
-function testStuff(testvar) {
-    console.log(testvar);
-}
+/*
+    Functions to be called when DOM is ready
+    Mostly dashboard calculations
+*/
+$(function() {
+    // article count
+    $("#article-count").text(localdata.length);
+
+    // img count
+    var imgCount = 0;
+    for (post in localdata) {
+        if (localdata[post].img != '') {
+            imgCount++;
+        }
+    }
+    $("#img-count").text(imgCount);
+
+    // newest and oldest post
+    chronoSorted = localdata.sort(function(a, b) {
+        return Date.parse(b.pubDate) - Date.parse(a.pubDate);
+    });
+    $("#newest-post").text(moment(chronoSorted[0].pubDate).format("MM/DD/YYYY HH:mm"));
+    $("#oldest-post").text(moment(chronoSorted[chronoSorted.length-1].pubDate).format("MM/DD/YYYY HH:mm"));
+
+    // go ahead and sort by date for default
+    sortPosts('date');
+});
--- routes/index.js
+++ routes/index.js
@@ -11,14 +11,6 @@ var parser = new Parser({
 });
 var router = express.Router();
 
-// padding function
-String.prototype.lpad = function(padString, length) {
-  var str = this;
-  while (str.length < length)
-      str = padString + str;
-  return str;
-}
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // user is logged in so go ahead and grab posts
@@ -43,11 +35,6 @@ router.get('/', function(req, res, next) {
                 } else if (posts.items[item].enclosure) {
                   posts.items[item].img = posts.items[item].enclosure.url;
                 }
-                // make the date the proper format
-                var date = new Date(posts.items[item].pubDate);
-                posts.items[item].prettyDate = (date.getMonth() + 1).toString().lpad('0', 2) + '/' + 
-                                                date.getDate().toString().lpad('0', 2) + '/' + date.getFullYear() + ' ' + 
-                                                date.getHours().toString().lpad('0', 2) + ':' + date.getMinutes().toString().lpad('0', 2);
                 combinedPosts.push(posts.items[item]);
               }
             }
@@ -55,7 +42,6 @@ router.get('/', function(req, res, next) {
             callback();
           });
         }, function(err) {
-          //console.log(combinedPosts);
           // calculate dashboard statistics to pass to frontend as well
           var articleCount = combinedPosts.length;
           
@@ -63,32 +49,7 @@ router.get('/', function(req, res, next) {
           if (articleCount == 0) {
             res.render('index', { newUser: true, user: req.user });
           } else {
-            var imgCount = 0;
-            for (post in combinedPosts) {
-              if (combinedPosts[post].img != '') {
-                imgCount++;
-              }
-            }
-            // initially sort all posts in chronological order
-            // can use this for dashboard as well
-            combinedPosts.sort(function(post1, post2) {
-              return Date.parse(post2.pubDate) - Date.parse(post1.pubDate);
-            });
-            var newestPost = combinedPosts[0].prettyDate;
-            var oldestPost = combinedPosts[combinedPosts.length - 1].prettyDate;
-            var dashboard = {articleCount: articleCount, imgCount: imgCount, newestPost: newestPost, oldestPost: oldestPost};
-            
-            // check if user wanted articles sorted by title or description instead of default date
-            if (req.query.sort == 'title') {
-              combinedPosts.sort(function(post1, post2) {
-                return post1.title.localeCompare(post2.title);
-              });
-            } else if (req.query.sort == 'desc') {
-              combinedPosts.sort(function(post1, post2) {
-                return post1.contentSnippet.localeCompare(post2.contentSnippet);
-              });
-            } 
-            res.render('index', { user: req.user, posts: combinedPosts, dash: dashboard });
+            res.render('index', { user: req.user, posts: combinedPosts });
           }
         });
       }
